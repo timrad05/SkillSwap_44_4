04bdd02f1b06f4c2500c35e13acc955edc46b1b6
gtyxf-lab <apa4a.r@yandex.ru>
2026-01-20 18:39:15 +0400

    feat(SidebarMenu): implement component logic and markup

--- src/shared/ui/SidebarMenu/SidebarMenu.tsx
+++ src/shared/ui/SidebarMenu/SidebarMenu.tsx
@@ -1,1 +1,26 @@
-export const SidebarMenu = {};
+import clsx from 'clsx';
+import { SidebarItem } from '../SidebarItem';
+import styles from './SidebarMenu.module.css';
+import type { TSidebarMenuProps } from './SidebarMenu.types';
+
+export const SidebarMenu = ({
+	items,
+	activeId,
+	onSelect,
+	className,
+}: TSidebarMenuProps) => {
+	return (
+		<div className={clsx(styles.menu, className)}>
+			{items.map((item) => (
+				<SidebarItem
+					key={item.id}
+					label={item.label}
+					icon={item.icon}
+					to={item.to}
+					active={!item.to ? activeId === item.id : undefined}
+					onClick={!item.to ? () => onSelect?.(item.id) : undefined}
+				/>
+			))}
+		</div>
+	);
+};
