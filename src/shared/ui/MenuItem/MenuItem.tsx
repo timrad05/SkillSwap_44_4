4d78c4aba6c84b0d3ef36833e15362bbfacf9643
Mihail Ujanin <MihailUj@yandex.ru>
2026-01-21 23:45:10 +0300

    feat: Добавлен компонент HeaderMenu и доработан компонент MenuItem

--- src/shared/ui/MenuItem/MenuItem.tsx
+++ src/shared/ui/MenuItem/MenuItem.tsx
@@ -2,17 +2,25 @@ import type { TMenuItemProps } from './MenuItem.types';
 import styles from './MenuItem.module.scss';
 import { NavLink } from 'react-router-dom';
 
-export const MenuItem = ({ label, to, onClick, className }: TMenuItemProps) => {
+export const MenuItem = ({
+	label,
+	to,
+	onClick,
+	className,
+	icon,
+}: TMenuItemProps) => {
 	if (to) {
 		return (
 			<NavLink to={to} className={`${styles.item} ${className}`}>
 				<span className={styles.label}>{label}</span>
+				{icon && <div className={styles.icon}>{icon}</div>}
 			</NavLink>
 		);
 	}
 	return (
 		<button onClick={onClick} className={`${styles.item} ${className}`}>
 			<span className={styles.label}>{label}</span>
+			{icon && <div className={styles.icon}>{icon}</div>}
 		</button>
 	);
 };
--- src/shared/ui/MenuItem/MenuItem.types.ts
+++ src/shared/ui/MenuItem/MenuItem.types.ts
@@ -1,8 +1,9 @@
-import type { MouseEventHandler } from 'react';
+import type { MouseEventHandler, ReactNode } from 'react';
 
 export type TMenuItemProps = {
 	label: string;
 	onClick?: MouseEventHandler<HTMLButtonElement>;
 	to?: string;
 	className?: string;
+	icon?: ReactNode;
 };
