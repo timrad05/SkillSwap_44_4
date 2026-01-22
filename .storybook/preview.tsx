971f635f1f40b059fbd897ce998fd78ea14253a3
Nick Boickov <nick.boickov@yandex.ru>
2026-01-22 10:34:31 +0300

    fix:скопированы файлы конфигурации из ветки develop

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -12,15 +12,6 @@ const preview: Preview = {
 			</Provider>
 		),
 	],
-
-	parameters: {
-		a11y: {
-			// 'todo' - show a11y violations in the test UI only
-			// 'error' - fail CI on a11y violations
-			// 'off' - skip a11y checks entirely
-			test: 'todo',
-		},
-	},
 };
 
 export default preview;
