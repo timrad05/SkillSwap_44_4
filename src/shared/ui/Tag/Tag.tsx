92b1c21031fd47bcfab6808f9fc0381deb18473f
Evgeniy <masquered@yandex.ru>
2026-01-22 16:31:39 +0500

    feat: add Tag

--- src/shared/ui/Tag/Tag.tsx
+++ src/shared/ui/Tag/Tag.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import type { TagProps } from './Tag.types';
-import styles from './Tag.module.css';
+import styles from './Tag.module.scss';
 
 export const Tag: React.FC<TagProps> = ({ text, color = 'plus' }) => {
 	const tagClass = `${styles.tag} ${styles[color] || styles.plus}`.trim();
