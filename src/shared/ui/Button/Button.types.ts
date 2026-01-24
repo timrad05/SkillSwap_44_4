15176f0079c1c1c5ac5f8016035a53bad03aff71
badsugar26 <badsugar26@yandex.ru>
2026-01-25 00:26:48 +0700

    feat: добавил виджит Cards. fix: добавил в кнопку дефолтное состояние Tertiary, которое не сделал сразу

--- src/shared/ui/Button/Button.types.ts
+++ src/shared/ui/Button/Button.types.ts
@@ -1,6 +1,6 @@
 import type { ButtonHTMLAttributes, ReactNode } from 'react';
 
-export type ButtonVariant = 'primary' | 'secondary';
+export type ButtonVariant = 'primary' | 'secondary' | 'tertiary';
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	children: ReactNode;
