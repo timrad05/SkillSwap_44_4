6fd2e8a0f1440392e45e1335ef4877195e549854
Nick Boickov <nick.boickov@yandex.ru>
2026-01-28 14:26:15 +0300

    подключение к app(закоменчено для тестов)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,49 +1,48 @@
-/* import styles from './app.module.scss';
+import styles from './app.module.scss';
 import { HomePage } from '../pages/HomePage';
-import { TestCards } from '../widgets/Cards/Cards.stories'; */
-import { ProfilePage } from '../pages/ProfilePage/ProfilePage';
-// Импортируем данные из SidebarMenu stories
-import { SidebarItems } from '../shared/ui/SidebarMenu/SidebarMenu.stories';
+import { TestCards } from '../widgets/Cards/Cards.stories';
+/* import { ProfilePage } from '../pages/ProfilePage/ProfilePage'; */
+/* import { SidebarItems } from '../shared/ui/SidebarMenu/SidebarMenu.stories'; */
 // import { NotFoundPage } from '../pages/NotFoundPage';
 // import { ProfilePage } from '../pages/ProfilePage';
 
 function App() {
 	return (
-		/* <div className={styles.app}>
-      <HomePage
-        headerProps={{}}
-        filterProps={{}}
-        cardsProps={{
-          title: 'Найдите своего ментора',
-          cards: TestCards,
-          viewAllText: 'Смотреть все',
-        }}
-        recommendedProps={{
-          cards: TestCards.slice(0, 9),
-        }}
-        footerProps={{
-          logoConfig: {
-            name: 'SkillSwap',
-            size: 'medium',
-          },
-          menuItems: [
-            { id: 'about', label: 'О проекте' },
-            { id: 'contact', label: 'Контакты' },
-            { id: 'privacy', label: 'Политика конфиденциальности' },
-            { id: 'skills', label: 'Все навыки' },
-            { id: 'blog', label: 'Блог' },
-            { id: 'terms', label: 'Пользовательское соглашение' },
-          ],
-        }}
-      />
-    </div> */
+		<div className={styles.app}>
+			<HomePage
+				headerProps={{}}
+				filterProps={{}}
+				cardsProps={{
+					title: 'Найдите своего ментора',
+					cards: TestCards,
+					viewAllText: 'Смотреть все',
+				}}
+				recommendedProps={{
+					cards: TestCards.slice(0, 9),
+				}}
+				footerProps={{
+					logoConfig: {
+						name: 'SkillSwap',
+						size: 'medium',
+					},
+					menuItems: [
+						{ id: 'about', label: 'О проекте' },
+						{ id: 'contact', label: 'Контакты' },
+						{ id: 'privacy', label: 'Политика конфиденциальности' },
+						{ id: 'skills', label: 'Все навыки' },
+						{ id: 'blog', label: 'Блог' },
+						{ id: 'terms', label: 'Пользовательское соглашение' },
+					],
+				}}
+			/>
+		</div>
 		// <>
 		//   <ServerErrorPage />
 		// </>
 		// <>
 		//   <NotFoundPage />
 		// </>
-		<ProfilePage
+		/* 		<ProfilePage
 			headerProps={{}}
 			sidebarProps={{
 				items: SidebarItems,
@@ -65,7 +64,7 @@ function App() {
 					{ id: 'terms', label: 'Пользовательское соглашение' },
 				],
 			}}
-		/>
+		/> */
 	);
 }
 
