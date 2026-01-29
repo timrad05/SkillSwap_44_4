529889097ba22f2ccc49757644cd8f86690fa00e
SanchezV <Vechkunin.ru@mail.ru>
2026-01-29 22:07:06 +0400

    feat: добавлен интерфейс skill

---
+++ src/entities/model/types.ts
@@ -0,0 +1,12 @@
+export interface Skill {
+	id: number;
+	category: string;
+	categoryId: number;
+	subcategory: string;
+	subcategoryId: number;
+	userId: number;
+	title: string;
+	description: string;
+	createdAt: string;
+	images: string[];
+}
