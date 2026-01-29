fcd927a5608676420f69d944db43e644b556ebf3
Evgeniy <masquered@yandex.ru>
2026-01-29 10:55:54 +0500

    feat: add RegisterPage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,22 @@
 import styles from './app.module.scss';
+/* Homepage */
 import { HomePage } from '../pages/HomePage';
 import { testCards } from '../widgets/Cards/Cards.data';
-/* import { RegisterPage } from '../pages/RegisterPage'; */
-// import { NotFoundPage } from '../pages/NotFoundPage';
+/* LoginPage */
+// import { LoginPage } from '../pages/LoginPage';
+/* RegisterPage */
+// import { RegisterPage } from '../pages/RegisterPage';
+/* ProfilePage */
 // import { ProfilePage } from '../pages/ProfilePage';
+// import { SidebarItems } from '../shared/ui/SidebarMenu/SidebarMenu.stories';
+/* NotFoundPage */
+// import { NotFoundPage } from '../pages/NotFoundPage';
+/* ServerErrorPage */
+// import { ServerErrorPage } from '../pages/ServerErrorPage';
 
 function App() {
 	return (
+		/* Homepage */
 		<div className={styles.app}>
 			<HomePage
 				headerProps={{}}
@@ -35,38 +45,48 @@ function App() {
 				}}
 			/>
 		</div>
-		// <>
-		//   <ServerErrorPage />
-		// </>
-		// <>
+		/* LoginPage */
+		// <div className={styles.app}>
+		// 	<LoginPage />
+		// </div>
+		/* RegisterPage */
+		// <div className={styles.app}>
+		// 	<RegisterPage />
+		// </div>
+		/* ProfilePage */
+		// <div className={styles.app}>
+		// 	<ProfilePage
+		// 		headerProps={{}}
+		// 		sidebarProps={{
+		// 			items: SidebarItems,
+		// 			activeId: 'profile',
+		// 			onSelect: (id) => console.log(`Selected: ${id}`),
+		// 		}}
+		// 		profileinfoProps={{}}
+		// 		footerProps={{
+		// 			logoConfig: {
+		// 				name: 'SkillSwap',
+		// 				size: 'medium',
+		// 			},
+		// 			menuItems: [
+		// 				{ id: 'about', label: 'О проекте' },
+		// 				{ id: 'contact', label: 'Контакты' },
+		// 				{ id: 'privacy', label: 'Политика конфиденциальности' },
+		// 				{ id: 'skills', label: 'Все навыки' },
+		// 				{ id: 'blog', label: 'Блог' },
+		// 				{ id: 'terms', label: 'Пользовательское соглашение' },
+		// 			],
+		// 		}}
+		// 	/>
+		// </div>
+		/* NotFoundPage */
+		// <div className={styles.app}>
 		//   <NotFoundPage />
-		// </>
-		/* 		<ProfilePage
-			headerProps={{}}
-			sidebarProps={{
-				items: SidebarItems,
-				activeId: 'profile',
-				onSelect: (id) => console.log(`Selected: ${id}`),
-			}}
-			profileinfoProps={{}}
-			footerProps={{
-				logoConfig: {
-					name: 'SkillSwap',
-					size: 'medium',
-				},
-				menuItems: [
-					{ id: 'about', label: 'О проекте' },
-					{ id: 'contact', label: 'Контакты' },
-					{ id: 'privacy', label: 'Политика конфиденциальности' },
-					{ id: 'skills', label: 'Все навыки' },
-					{ id: 'blog', label: 'Блог' },
-					{ id: 'terms', label: 'Пользовательское соглашение' },
-				],
-			}}
-		/> */
-		//
-		/* <RegisterPage /> */
+		// </div>
+		/* ServerErrorPage */
+		// <div className={styles.app}>
+		// 	 <ServerErrorPage />
+		// </div>
 	);
 }
-
 export default App;
