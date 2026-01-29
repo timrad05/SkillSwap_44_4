fcc375023283c3e3bfc993073118c17c43cd45a0
Evgeniy <masquered@yandex.ru>
2026-01-29 10:43:42 +0500

    fix: update styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,20 @@
 import styles from './app.module.scss';
+/* Homepage */
 import { HomePage } from '../pages/HomePage';
 import { testCards } from '../widgets/Cards/Cards.data';
+/* LoginPage */
 // import { LoginPage } from '../pages/LoginPage';
-// import { NotFoundPage } from '../pages/NotFoundPage';
+/* ProfilePage */
 // import { ProfilePage } from '../pages/ProfilePage';
+// import { SidebarItems } from '../shared/ui/SidebarMenu/SidebarMenu.stories';
+/* NotFoundPage */
+// import { NotFoundPage } from '../pages/NotFoundPage';
+/* ServerErrorPage */
+// import { ServerErrorPage } from '../pages/ServerErrorPage';
 
 function App() {
 	return (
+		/* Homepage */
 		<div className={styles.app}>
 			<HomePage
 				headerProps={{}}
@@ -35,38 +43,44 @@ function App() {
 				}}
 			/>
 		</div>
+		/* LoginPage */
 		// <div className={styles.app}>
 		// 	<LoginPage />
 		// </div>
-		// <>
-		//   <ServerErrorPage />
-		// </>
-		// <>
+		/* ProfilePage */
+		// <div className={styles.app}>
+		// 	<ProfilePage
+		// 		headerProps={{}}
+		// 		sidebarProps={{
+		// 			items: SidebarItems,
+		// 			activeId: 'profile',
+		// 			onSelect: (id) => console.log(`Selected: ${id}`),
+		// 		}}
+		// 		profileinfoProps={{}}
+		// 		footerProps={{
+		// 			logoConfig: {
+		// 				name: 'SkillSwap',
+		// 				size: 'medium',
+		// 			},
+		// 			menuItems: [
+		// 				{ id: 'about', label: 'О проекте' },
+		// 				{ id: 'contact', label: 'Контакты' },
+		// 				{ id: 'privacy', label: 'Политика конфиденциальности' },
+		// 				{ id: 'skills', label: 'Все навыки' },
+		// 				{ id: 'blog', label: 'Блог' },
+		// 				{ id: 'terms', label: 'Пользовательское соглашение' },
+		// 			],
+		// 		}}
+		// 	/>
+		// </div>
+		/* NotFoundPage */
+		// <div className={styles.app}>
 		//   <NotFoundPage />
-		// </>
-		// <ProfilePage
-		// 	headerProps={{}}
-		// 	sidebarProps={{
-		// 		items: SidebarItems,
-		// 		activeId: 'profile',
-		// 		onSelect: (id) => console.log(`Selected: ${id}`),
-		// 	}}
-		// 	profileinfoProps={{}}
-		// 	footerProps={{
-		// 		logoConfig: {
-		// 			name: 'SkillSwap',
-		// 			size: 'medium',
-		// 		},
-		// 		menuItems: [
-		// 			{ id: 'about', label: 'О проекте' },
-		// 			{ id: 'contact', label: 'Контакты' },
-		// 			{ id: 'privacy', label: 'Политика конфиденциальности' },
-		// 			{ id: 'skills', label: 'Все навыки' },
-		// 			{ id: 'blog', label: 'Блог' },
-		// 			{ id: 'terms', label: 'Пользовательское соглашение' },
-		// 		],
-		// 	}}
-		// />
+		// </div>
+		/* ServerErrorPage */
+		// <div className={styles.app}>
+		// 	 <ServerErrorPage />
+		// </div>
 	);
 }
 export default App;
