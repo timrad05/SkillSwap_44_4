0362ad388801e128e5907d32a2490351012b266c
Mihail Ujanin <MihailUj@yandex.ru>
2026-02-04 02:59:07 +0300

    feat: Доработал форму профиля, подключил данные на страницу

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -4,6 +4,15 @@ import { Footer } from '../../widgets/Footer';
 import { Header } from '../../widgets/Header';
 import type { ProfilePageProps } from './ProfilePage.types';
 import styles from './ProfilePage.module.scss';
+import { useEffect, useState } from 'react';
+import {
+	getCurrentUser,
+	setCurrentUser,
+	type User,
+} from '../../entities/user/model';
+import type { City } from '../../entities/city/model/types';
+import { getCities } from '../../api/cities';
+import { useNavigate } from 'react-router-dom';
 
 export const ProfilePage = ({
 	headerProps = {},
@@ -15,6 +24,44 @@ export const ProfilePage = ({
 	footerProps = {},
 	profileinfoProps = {},
 }: ProfilePageProps) => {
+	const navigate = useNavigate();
+	const [user, setUser] = useState<User | null>(null);
+	const [cities, setCities] = useState<City[]>([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		//Убрать после реализации авторизации
+		setCurrentUser({
+			id: 1,
+			name: 'Иван Васильевич',
+			email: 'mail@m.ru',
+			avatar: 'https://i.pravatar.cc/150?img=11',
+			cityId: 1,
+			dateOfBirth: '1981.10.25',
+			canTeach: [18],
+			wantToLearn: [24, 32],
+			gender: 'male',
+			about:
+				'Инженер-механик, увлекаюсь рыбалкой и походами на природу. Ценю честность.',
+			registrationDate: '2026-01-19',
+		});
+		////////////////////////////////////////////
+		getCities()
+			.then((data) => {
+				setCities(data);
+			})
+			.catch((err) => console.error('Ошибка загрузки данных:', err))
+			.finally(() => setIsLoading(false));
+		setUser(getCurrentUser());
+	}, []);
+
+	if (isLoading) {
+		return <div className={styles.page}>Загрузка...</div>;
+	}
+	if (!isLoading && !user) {
+		navigate('/login', { replace: true });
+	}
+
 	return (
 		<div className={styles.page}>
 			<Header {...headerProps} />
@@ -24,7 +71,7 @@ export const ProfilePage = ({
 					<SidebarMenu {...sidebarProps} />
 				</aside>
 				<div className={styles.profile}>
-					<ProfileInfo {...profileinfoProps} />
+					<ProfileInfo {...profileinfoProps} cities={cities} user={user} />
 				</div>
 			</main>
 			<Footer {...footerProps} />
--- src/shared/ui/ProfileInfo/ProfileInfo.types.ts
+++ src/shared/ui/ProfileInfo/ProfileInfo.types.ts
@@ -1,4 +1,9 @@
+import type { City } from '../../../entities/city/model/types';
+import type { User } from '../../../entities/user/model';
+
 export interface ProfileInfoProps {
 	className?: string;
 	avatarUrl?: string;
+	user?: User | null;
+	cities?: City[];
 }
